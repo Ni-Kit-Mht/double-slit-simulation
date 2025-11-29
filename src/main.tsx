a6c0b3a04c580900ebd5b68f3c6bcf30824ac918
ni-kit-mht <nikitmht4@gmail.com>
2025-11-29 15:43:03 +0545

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import WaveOpticsSimulation from "./WaveOpticsSimulation";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <WaveOpticsSimulation />
